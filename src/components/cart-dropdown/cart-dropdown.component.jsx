04a16396b191adec234cdb1c71e23f231f8142b9
chibokamc <chibokachinedum@gmail.com>
2022-04-19 22:40:04 +0100

    Added product card, cart dropdown, cart counter

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,21 @@
-import Button from '../button/button.component'
-import './cart-dropdown.styles.scss'
+import { useContext } from "react";
+import Button from "../button/button.component";
+import { CartContext } from "../../contexts/cart.context";
+import "./cart-dropdown.styles.scss";
+import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = () => {
-  return (
-    <div className='cart-dropdown-container'>
-      <div className='cart-items'/>
-      <Button >GO TO CHECKOUT</Button>
-    </div>
-  )
+	const { cartItems } = useContext(CartContext);
+	return (
+		<div className="cart-dropdown-container">
+			<div className="cart-items">
+				{cartItems.map((item) => (
+					<CartItem key={item.id} cartItem={item} />
+				))}
+			</div>
+			<Button>GO TO CHECKOUT</Button>
+		</div>
+	);
+};
 
-}
-
-export default CartDropdown
+export default CartDropdown;
