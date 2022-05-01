aa6da5dac3287c34536dab1d8cd88653e1dceda8
chibokamc <chibokachinedum@gmail.com>
2022-05-01 16:44:18 +0100

    Styled components for directory-item, form-input, sign-in form, cart-icon & cart-dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,25 +2,27 @@ import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../button/button.component";
 import { CartContext } from "../../contexts/cart.context";
-import "./cart-dropdown.styles.scss";
+import {CartDropdownContainer, EmptyMessage, CartItems} from "./cart-dropdown.styles";
 import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = () => {
 	const { cartItems } = useContext(CartContext);
-	const navigate = useNavigate()
+	const navigate = useNavigate();
 
 	const navigateToCheckoutHandler = () => {
-		navigate('/checkout')
-	}
+		navigate("/checkout");
+	};
 	return (
-		<div className="cart-dropdown-container">
-			<div className="cart-items">
-				{cartItems.map((item) => (
-					<CartItem key={item.id} cartItem={item} />
-				))}
-			</div>
+		<CartDropdownContainer>
+			<CartItems>
+				{cartItems.length ? (
+					cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+				) : (
+					<EmptyMessage>Your cart is empty</EmptyMessage>
+				)}
+			</CartItems>
 			<Button onClick={navigateToCheckoutHandler}>GO TO CHECKOUT</Button>
-		</div>
+		</CartDropdownContainer>
 	);
 };
 
--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,19 +1,22 @@
-// import React from "react";
-import "./directory-item.styles.scss";
+import {
+	BackgroundImage,
+	Body,
+	DirectoryItemContainer,
+} from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
 	const { title, imageUrl } = category;
 	return (
-		<div className="directory-item-container">
-			<div
-				className="background-image"
-				style={{ backgroundImage: `url(${imageUrl})` }}
+		<DirectoryItemContainer>
+			<BackgroundImage
+				imageUrl={imageUrl}
+				// style={{ backgroundImage: `url(${imageUrl})` }}
 			/>
-			<div className="body">
+			<Body>
 				<h2>{title.toUpperCase()}</h2>
 				<p>Shop Now</p>
-			</div>
-		</div>
+			</Body>
+		</DirectoryItemContainer>
 	);
 };
 
